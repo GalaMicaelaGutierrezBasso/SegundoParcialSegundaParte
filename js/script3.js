7f547c269f871ba1bcd85674aa51e098ec106371
GalaMicaelaGutierrezBasso <galagutierrez3.5@gmail.com>
2026-07-11 20:29:31 -0300

    js
    organizacion script3.js

---
+++ js/script3.js
@@ -0,0 +1,8 @@
+// Sistema para controlar el funcionamiento de las luces móviles de las obras de Rafael Lozano-Hemmer
+// El consumo diario total en kWh entre todas las obras y el consumo diario promedio por obra
+
+// La obra con mayor tiempo de funcionamiento y su costo diario
+
+// El porcentaje de obras que utilizan más de 20 luces
+
+// Habilitar un botón para reiniciar
