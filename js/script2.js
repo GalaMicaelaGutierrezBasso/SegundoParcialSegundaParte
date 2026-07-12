5501ffcd2f1da34e31591e2330947d1aa56bede8
GalaMicaelaGutierrezBasso <galagutierrez3.5@gmail.com>
2026-07-12 20:14:59 -0300

    js
    script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,3 @@
-// Declarar las variable: los datos curiosos al azar
 let datosCuriosos = [
 "Rafael Lozano-Hemmer es un artista mexicano-canadiense especializado en instalaciones interactivas en espacios públicos.",
 "Su obra combina tecnología avanzada como sensores biométricos, robótica y datos en tiempo real.",
@@ -12,15 +11,11 @@ let datosCuriosos = [
 "Utiliza la luz como lenguaje poético para explorar temas de identidad, memoria y presencia."
 ];
 
-// Con una funcion mostrar un dato curioso al azar, que de todos los datos (el largo del array anterior) elija uno al azar (y redondea el numero entero mas cercano y asi es especifico el dato curioso)
-// Despues obtener el elemento con el id "datocurioso" y cambiar su contenido al dato curioso obtenido anteriormente
 function mostrarDato() {
     let datos = Math.round(Math.random() * datosCuriosos.length);
     document.querySelector("#datocurioso").textContent = datosCuriosos[datos];
     }
 
-// Obtener el elemento con el id "botondato" y que al hacer click en el botón nos mostrara otro dato curioso
 document.querySelector("#botondato").addEventListener("click", mostrarDato);
 
-// Para que no haga falta hacer click en el boton para que se muestre otro dato, ya sea al cargar la pagina o recargarla
 mostrarDato();
